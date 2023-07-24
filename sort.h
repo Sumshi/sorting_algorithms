4b9fcc12319f31f51a6964688e0e668f911fd5a1
Sumshi <sumayasomow@gmail.com>
2023-07-24 12:16:38 +0000

    done

--- sort.h
+++ sort.h
@@ -45,6 +45,6 @@ void heap_sort(int *array, size_t size);
 void swap_node(int *array, size_t root, size_t high, size_t size);
 
 void radix_sort(int *array, size_t size);
-int get_max(int *array, size_t size);
-void count_sort(int *array, size_t size, int digit);
+int get_max(int *arr, size_t size);
+void count_sort(int *arr, size_t size, int digit);
 #endif
