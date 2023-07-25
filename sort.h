59bec66ef3ca0ae38ed2a6fe47e40efa5160c5d6
Sumshi <sumayasomow@gmail.com>
2023-07-25 08:02:54 +0000

    bitonic sort

--- sort.h
+++ sort.h
@@ -52,4 +52,9 @@ void count_sort(int *array, size_t size, int digit);
 size_t partition_ho(int *array, ssize_t lo, ssize_t hi, size_t size);
 void quicksort(int *array, ssize_t lo, ssize_t hi, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+
+void bitonic_sort(int *array, size_t size);
+void bitonic_swap(char up, int *array, size_t size);
+void bitonic_merge(char up, int *array, size_t size);
+void bit_sort(char up, int *array, size_t size, size_t t);
 #endif
